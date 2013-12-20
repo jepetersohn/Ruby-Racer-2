dcacc2fcb1964f167c1771626d8d0584d23e265d
Strand McCutchen <strand@bettermistak.es>
2013-12-20 15:21:23 -0800

    Whitespace pass

--- source/ruby_racer.rb
+++ source/ruby_racer.rb
@@ -5,20 +5,20 @@ class RubyRacer
 
   def initialize(players, length = 30)
   end
-  
-  # Returns +true+ if one of the players has reached 
+
+  # Returns +true+ if one of the players has reached
   # the finish line, +false+ otherwise
   def finished?
   end
-  
+
   # Returns the winner if there is one, +nil+ otherwise
   def winner
   end
-  
+
   # Rolls the dice and advances +player+ accordingly
   def advance_player!(player)
   end
-  
+
   # Prints the current game board
   # The board should have the same dimensions each time
   # and you should use the "reputs" helper to print over
@@ -38,11 +38,11 @@ until game.finished?
   players.each do |player|
     # This moves the cursor back to the upper-left of the screen
     move_to_home!
-  
+
     # We print the board first so we see the initial, starting board
     game.print_board
     game.advance_player!(player)
-    
+
     # We need to sleep a little, otherwise the game will blow right past us.
     # See http://www.ruby-doc.org/core-1.9.3/Kernel.html#method-i-sleep
     sleep(0.5)
