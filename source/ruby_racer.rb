25c6e4ec64a415c39101ae75a68534a19a71232b
Matt Baker <mbaker.pdx@gmail.com>
2014-06-05 15:05:24 -0500

    Rspec conversion

--- source/ruby_racer.rb
+++ source/ruby_racer.rb
@@ -1,4 +1,4 @@
-require_relative 'racer_utils'
+require 'die.rb'
 
 class RubyRacer
   attr_reader :players, :length
@@ -24,31 +24,24 @@ class RubyRacer
   # and you should print over the previous board
   def print_board
   end
-end
 
-players = ['a', 'b']
 
-game = RubyRacer.new(players)
-
-# This clears the screen, so the fun can begin
-clear_screen!
-
-until game.finished?
-  players.each do |player|
-    # This moves the cursor back to the upper-left of the screen
-    move_to_home!
-
-    # We print the board first so we see the initial, starting board
-    game.print_board
-    game.advance_player!(player)
-
-    # We need to sleep a little, otherwise the game will blow right past us.
-    # See http://www.ruby-doc.org/core-1.9.3/Kernel.html#method-i-sleep
-    sleep(0.5)
+  #Clears the content on the screen. Ah, a fresh canvas.
+  #You don't have to test this
+  def clear_screen!
+    print "\e[2J"
   end
+
+  #Moves the "cursor" back to the upper left.
+  #You don't have to test this
+  def move_to_home!
+    print "\e[H"
+  end
+
+  #You don't have to test this
+  def reset_screen!
+    clear_screen!
+    move_to_home!
+  end
+
 end
-
-# The game is over, so we need to print the "winning" board
-game.print_board
-
-puts "Player '#{game.winner}' has won!"
